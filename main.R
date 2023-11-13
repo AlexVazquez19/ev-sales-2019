d53da8c5eb3f724a203208bfaaa18100d4a2ba47
AlexVazquez19 <86645097+AlexVazquez19@users.noreply.github.com>
2023-11-13 13:08:23 -0800

    Create main.R

---
+++ main.R
@@ -0,0 +1,13 @@
+# load libraries
+library(readr)
+
+# clear environment
+rm(list = ls())
+
+# set working directory (edit to your directory)
+setwd("/Users/alejandrovazquez/Desktop/econ121/ev-sales-2019")
+
+# load data
+evsales <- read_csv("ev_sales_data_2019.csv")
+
+
